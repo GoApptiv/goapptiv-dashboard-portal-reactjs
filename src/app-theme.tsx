16eee15d72465dbda320ac13ad24928e07cef6ae
sharan-sublam <sharan.raj@goapptiv.com>
2023-10-05 17:46:38 +0530

    common files

---
+++ src/app-theme.tsx
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    info: {
+      main: "#6B6B6B",
+    },
+    primary: {
+      main: "#212121",
+    },
+    secondary: {
+      main: "#14008D",
+    },
+  },
+});
+
+export default theme;
